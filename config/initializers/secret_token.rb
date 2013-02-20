98436a47cb651fbf57c80c1fbd163416a72adf18
Rex Feng <rexfeng@gmail.com>
2013-02-20 00:49:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GotTweet::Application.config.secret_token = '6f3d2175973b4004d827a91a038e3a0ed351ea5b73f137c9dd19832547f7a0dc7ab4f42d673f17d5282f6c6fa48913d6400a9c84e674c3dc7a263d68fb6eed9b'
